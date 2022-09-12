63e9a05a5473a69750f2724c59500b54f99a0aab
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-09-12 14:55:13 -0300

    feat(tasks): fixed edit date task and remove calendar button

--- src/components/TasksList/EditDateTask/EditDateTask.tsx
+++ src/components/TasksList/EditDateTask/EditDateTask.tsx
@@ -1,15 +1,12 @@
 import { ChangeEvent, useState } from "react";
 
 import { format, parse, parseISO } from "date-fns";
-import { Calendar, FloppyDisk } from "phosphor-react";
 
 import DateInput from "../../DateInput";
+import { Task } from "../../../types/Task";
 import { DATE_INPUT_MASK } from "../../DateInput/DateInput";
 
 import styles from "./EditDateTask.module.css";
-
-import { Task } from "../../../types/Task";
-import TaskItemAction from "../TaskItemAction";
 
 function parseDate(value: string) {
   return parse(value, "HH:mm", new Date());
@@ -21,13 +18,16 @@ type Props = {
 };
 
 export default function EditDateTask({ task, onEditDateTask }: Props) {
-  const [isEditDateTask, setIsEditDateTask] = useState(false);
   const [startDate, setStartDate] = useState<Date | null>(
     task.startDate ? parseISO(task.startDate) : null
   );
   const [endDate, setEndDate] = useState<Date | null>(
     task.endDate ? parseISO(task.endDate) : null
   );
+
+  const isOverdue = task.endDate
+    ? parseISO(task.endDate).getTime() < new Date().getTime()
+    : false;
 
   function handleChangeStartDate(event: ChangeEvent<HTMLInputElement>) {
     if (event.currentTarget.value.length === 0) {
@@ -53,55 +53,33 @@ export default function EditDateTask({ task, onEditDateTask }: Props) {
     }
   }
 
-  function handleToggleEditDateTaskVisibility() {
-    setIsEditDateTask((state) => !state);
-  }
-
   function handleEditDateTask() {
     onEditDateTask({
       ...task,
       startDate: startDate?.toISOString(),
       endDate: endDate?.toISOString(),
     });
-
-    setIsEditDateTask(false);
   }
 
   return (
-    <>
-      <div className={styles.editDateTask}>
-        <DateInput
-          required
-          disabled={!isEditDateTask}
-          defaultValue={startDate ? format(startDate, "HH:mm") : ""}
-          placeholder="Start Date"
-          onChange={handleChangeStartDate}
-        />
-        <DateInput
-          required
-          disabled={!isEditDateTask}
-          defaultValue={endDate ? format(endDate, "HH:mm") : ""}
-          placeholder="Due Date"
-          onChange={handleChangeEndDate}
-        />
-      </div>
-      {isEditDateTask ? (
-        <TaskItemAction
-          title="Salvar datas"
-          className={styles.editDateTask}
-          onClick={handleEditDateTask}
-        >
-          <FloppyDisk size={16} />
-        </TaskItemAction>
-      ) : (
-        <TaskItemAction
-          title="Editar data"
-          className={styles.editDateTask}
-          onClick={handleToggleEditDateTaskVisibility}
-        >
-          <Calendar size={16} />
-        </TaskItemAction>
-      )}
-    </>
+    <div className={styles.editDateTask}>
+      <DateInput
+        required
+        defaultValue={startDate ? format(startDate, "HH:mm") : ""}
+        placeholder="hh:mm"
+        onChange={handleChangeStartDate}
+        onBlur={handleEditDateTask}
+      />
+      -
+      <DateInput
+        required
+        checkedAt={task.checkedAt}
+        overDue={isOverdue}
+        defaultValue={endDate ? format(endDate, "HH:mm") : ""}
+        placeholder="hh:mm"
+        onChange={handleChangeEndDate}
+        onBlur={handleEditDateTask}
+      />
+    </div>
   );
 }
