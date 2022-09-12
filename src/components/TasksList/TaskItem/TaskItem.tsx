e9b58157a47b3a876e1642a5abd663835dff8d75
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-09-12 14:15:16 -0300

    feat(task-item): use edit date task in task item

--- src/components/TasksList/TaskItem/TaskItem.tsx
+++ src/components/TasksList/TaskItem/TaskItem.tsx
@@ -1,22 +1,28 @@
-import { Calendar, Trash } from "phosphor-react";
-import cn from "classnames";
+import { ChangeEvent, useState } from "react";
+import { Trash } from "phosphor-react";
 
 import Radio from "../../Radio";
 import { Task } from "../../../types/Task";
 
 import styles from "./TaskItem.module.css";
+import EditDateTask from "../EditDateTask/EditDateTask";
+import TaskItemAction from "../TaskItemAction";
 
 type Props = {
   task: Task;
+  onEditDateTask: (task: Task) => void;
   onCheckedChange: (task: Task) => void;
   onDeleteTask: (task: Task) => void;
 };
 
 export default function TaskItem({
   task,
+  onEditDateTask,
   onCheckedChange,
   onDeleteTask,
 }: Props) {
+  const [newTaskContent, setNewTaskContent] = useState(task.content);
+
   function handleCheckedChange() {
     onCheckedChange(task);
   }
@@ -25,27 +31,31 @@ export default function TaskItem({
     onDeleteTask(task);
   }
 
+  function handleNewTaskContentChange(
+    event: ChangeEvent<HTMLParagraphElement>
+  ) {
+    setNewTaskContent(event.target.textContent ?? "");
+  }
+
   return (
     <li className={styles.taskItem}>
-      <Radio checked={Boolean(task.checkedAt)} onClick={handleCheckedChange} />
-      <p className={task.checkedAt ? styles.checked : undefined}>
-        {task.content}
-      </p>
+      <Radio checked={Boolean(task.checkedAt)} onChange={handleCheckedChange} />
+      <p
+        className={task.checkedAt ? styles.checked : undefined}
+        contentEditable
+        onInput={handleNewTaskContentChange}
+        onBlur={() => onEditDateTask({ ...task, content: newTaskContent })}
+        dangerouslySetInnerHTML={{ __html: newTaskContent }}
+      />
       <div className={styles.actions}>
-        <button
-          title="Editar data"
-          className={cn([styles.actionButton, styles.editDateTask])}
-          onClick={handleDeleteTask}
-        >
-          <Calendar size={16} />
-        </button>
-        <button
+        <EditDateTask task={task} onEditDateTask={onEditDateTask} />
+        <TaskItemAction
           title="Excluir tarefa"
-          className={cn([styles.actionButton, styles.deleteTask])}
+          className={styles.deleteTask}
           onClick={handleDeleteTask}
         >
           <Trash size={16} />
-        </button>
+        </TaskItemAction>
       </div>
     </li>
   );
